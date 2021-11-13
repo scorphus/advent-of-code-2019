00aa959053e1207c121c47f8bb7437449d8858d1
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-11-13 22:45:39 +0100

    cli: Improve usability even if by a small margin

--- lib/advent_of_code_2019/cli.ex
+++ lib/advent_of_code_2019/cli.ex
@@ -8,15 +8,16 @@ defmodule AdventOfCode2019.CLI do
 
   @spec main(Enumerable.t()) :: any()
   def main([]) do
-    IO.puts("Missing day.part! Ex.: ./advent_of_code_2019 < day01/input 1.1")
+    IO.puts("Missing day.part! Ex.: ./advent_of_code_2019 < data/day01 1.1")
     System.halt(1)
   end
 
   def main(args) do
-    IO.puts("Your answer is:")
+    args = Enum.join(args, ".")
+    IO.puts("Answer for #{args} is:")
 
     IO.stream(:stdio, :line)
-    |> AdventOfCode2019.stream_lines(Enum.join(args, "."))
+    |> AdventOfCode2019.stream_lines(args)
     |> IO.puts()
   end
 end
